365f0deeceb1578474af5476e7fd265d6458c552
Abdurrahman Rahim <104781398+abdurrahmanr@users.noreply.github.com>
2024-02-12 18:17:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 mongoose.connect(process.env["MONGO_URI"]);
-const Url = require("./models/UrlModel");
+const Url = require("./models/Url");
 const regex = require("./utils/urlValidator");
 
 app.use(bodyParser.urlencoded({ extended: "false" }));
